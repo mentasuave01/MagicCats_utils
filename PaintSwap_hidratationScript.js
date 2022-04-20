20523dbc055bb7da2a6923a23af23973aba46e26
mentasuave01 <60361959+mentasuave01@users.noreply.github.com>
2022-04-20 15:31:07 +0200

    Update PaintSwap_hidratationScript.js

--- PaintSwap_hidratationScript.js
+++ PaintSwap_hidratationScript.js
@@ -1,41 +1,4 @@
-import {catData} from './catDatabase.js' //Custom dataset to hidrate request from
-// import fetch from 'node-fetch' ignoreme if not using node
 
-async function catsData()  {
-    const res = await fetch(`https://api.paintswap.finance/v2/sales?collections=0x2ab5c606a5aa2352f8072b9e2e8a213033e2c4c9&numToFetch=1000`);
-    const paintRes = (await res.json());
-    let paintCats = await paintRes.sales;
-    return paintCats;
-}
-
-function hidratePaintSwapData(paintCats){
-    
-    for  (let i = 0; i < paintCats.length; i++) {
-            paintCats[i].score = catData[paintCats[i].tokenId].score
-            paintCats[i].MPratio = catData[paintCats[i].tokenId].score/(paintCats[i].price/1e18)
-    }
-
-    return paintCats;
-}
-
-function sortByMPratio( paintCatsWithScore) {
-    paintCatsWithScore.sort(function(a, b) {
-        return b.MPratio - a.MPratio;
-    });
-    return paintCatsWithScore;
-}
-
-
-async function main() {
-    const paintCats = await catsData();
-    const paintCatsWithScore = hidratePaintSwapData(paintCats);
-    const paintCatsWithScore_sortedByScore = sortByMPratio(await paintCatsWithScore);
-    //console.log(await paintCatsWithScore_sortedByScore); just for testing
-    return paintCatsWithScore_sortedByScore;    
-}
-
-main();
-=======
 import {catData} from './catDatabase.js' //Custom dataset to hidrate request from
 // import fetch from 'node-fetch' ignoreme if not using node
 
